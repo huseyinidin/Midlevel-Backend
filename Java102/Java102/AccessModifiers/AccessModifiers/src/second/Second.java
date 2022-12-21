3ba2c679392a021f2e6ada3f9f164c13b3f5e0fd
Hüseyin idin <huseyinidinn@gmail.com>
2022-12-21 18:27:53 +0300

    Public Access Modifier

---
+++ Java102/Java102/AccessModifiers/AccessModifiers/src/second/Second.java
@@ -0,0 +1,14 @@
+package second;
+
+import first.First;
+
+public class Second {
+    public static void main(String[] args) {
+
+        // Public
+
+        First b = new First("Kodluyoruz");
+        //System.out.println(b.str);
+        b.show();
+    }
+}
