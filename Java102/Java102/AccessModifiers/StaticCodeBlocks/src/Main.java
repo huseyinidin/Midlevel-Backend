1d2745d224f8a2e2430a860a16d42f0d0cf6f02d
Hüseyin idin <huseyinidinn@gmail.com>
2022-12-21 22:41:49 +0300

    Static Code Blocks

---
+++ Java102/Java102/AccessModifiers/StaticCodeBlocks/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        User u1 = new User("Kelly Cross");
+        User u2 = new User("Anna Karen");
+
+        System.out.println(User.counter);
+    }
+}
