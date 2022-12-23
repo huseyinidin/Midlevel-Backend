283dd2ac04d0a80da9b04edee61cac3042bf9938
Hüseyin idin <huseyinidinn@gmail.com>
2022-12-23 15:42:39 +0300

    Abstract

---
+++ Java102/Java102/ObjectOrientedPrograming/Abstraction/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Employee e1 = new Employee("John", "Smith", "5999999999", "John@smith.com");
+
+        //Academic a1 = new Academic("Ella", "Watson", "5999999998", "Ella@watson.com", "Computer Science", "Professor");
+
+        //Officer o1 = new Officer("Ricky", "Stone", "5999999994", "Ricky@stone.com", "Information Technology", "8.00 - 18:00");
+
+        Instructor i1 = new Instructor("Lucy", "Page", "5999999997", "Lucy@page.com", "Computer Science", "Instructor", "I555");
+
+        i1.entryLesson("3:00");
+
+    }
+}
