aa60f0f14e7c4d96cbf84145011d6d070ab1c8b4
Hüseyin idin <huseyinidinn@gmail.com>
2022-12-23 15:38:21 +0300

    Abstract - Abstract method

---
+++ Java102/Java102/ObjectOrientedPrograming/Abstraction/src/Academic.java
@@ -0,0 +1,39 @@
+public abstract class Academic extends Employee {
+
+    private String department;
+    private String degree;
+
+    public Academic(String name, String surname, String phoneNumber, String email, String department, String degree) {
+        super(name, surname, phoneNumber, email);
+        this.department = department;
+        this.degree = degree;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public void lesson() {
+        System.out.println(this.getName() + " entered the class!");
+    }
+
+    @Override
+    public void entry() {
+        System.out.println(this.getName() + " entered through Gate A.");
+    }
+
+    // Abstract method
+    public abstract void entryLesson(String lessonTime);
+}
